1e3ebf51b60159be0367f6cfa8cc3757d60a9ddf
Andrés Sarmiento <acamilo707@gmail.com>
2023-02-13 14:39:46 -0500

    A Build task and a development (serve) task were defined

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require('gulp');
+const tasks = require('./gulp/tasks/index');
+const workflows = require('./gulp/workflows/index');
+
+const templateData = {}
+
+const pageBuilderTasks = workflows.pageBuilder(templateData);
+
+const babelTask = () => tasks.babel({
+    source: './dev/src/scripts/**/*.js',
+    destinationFolder: './dist/src/scripts'
+});
+
+const babelCompressor = () => tasks.imgCompressor({
+    source: './dev/src/res/images/**/*',
+    destinationFolder: './dist/src/res/images'
+})
+
+/* Tasks */
+
+gulp.task('serve', async () => {
+    gulp.watch('./dev/src/scripts/**/*.js', { events: 'all' }, babelTask);
+    gulp.watch('./dev/src/res/images/**/*', { events: 'all' }, babelTask);
+    gulp.watch('./dev/**/*.html', { events: 'all' }, gulp.parallel(...pageBuilderTasks));
+    gulp.watch('./dev/src/styles/**/*.css', { events: 'all' }, gulp.parallel(...pageBuilderTasks));
+});
+
+gulp.task('build', gulp.parallel(
+    babelTask,
+    babelCompressor,
+    pageBuilderTasks
+));
